d84ff4a9f8619b31abe6878e60a12b8bec060da7
Gabriel <gabriel.benoit@student.unamur.be>
2023-01-23 13:53:45 +0100

    fixing PDF layout and style

--- src/pdf_loader/mod.rs
+++ src/pdf_loader/mod.rs
@@ -16,7 +16,25 @@ pub async fn pdf_scrapper(url: &str) -> Result<Vec<u8>, Box<dyn std::error::Erro
         }
     });
     let page = browser.new_page(url).await?;
-    let params = PrintToPdfParams::default();
+    let params = PrintToPdfParams {
+        landscape: None,
+        display_header_footer: Some(false),
+        print_background: Some(true),
+        scale: None,
+        paper_width: None,
+        paper_height: None,
+        margin_top: Some(1.38),
+        margin_bottom: Some(2.3),
+        margin_left: Some(1.4),
+        margin_right: Some(1.4),
+        page_ranges: None,
+        ignore_invalid_page_ranges: None,
+        header_template: None,
+        footer_template: None,
+        prefer_css_page_size: Some(true),
+        transfer_mode: None,
+    };
+
     let pdf = page.pdf(params).await?;
     browser.close().await?;
     Ok(pdf)
